bf46a1ca02563fd374b62c9bb3ef1a5f5c3b3878
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-12-05 12:14:19 +0100

    test: add unit tests for custom fetch hook

---
+++ src/components/useFetchData/useFetchData.test.jsx
@@ -0,0 +1,70 @@
+import { describe, it, expect, beforeEach } from "vitest";
+import { renderHook, waitFor } from '@testing-library/react';
+import useFetchData from "./useFetchData";
+
+beforeEach(() => {
+	global.fetch = vi.fn();
+})
+
+afterEach(() => {
+	vi.restoreAllMocks();
+})
+
+describe("useFetchData custom hook", () => {
+	it("sets loading to true", () => {
+		const mockData = { message: 'Success' };
+		global.fetch.mockResolvedValueOnce({
+			ok: true,
+			json: async () => (mockData)
+		})
+
+		const { result } = renderHook(() => useFetchData('/mock-url'));
+
+		expect(result.current.loading).toBe(true);
+	});
+
+	it("fetches data successfully", async () => {
+		const mockData = { message: 'Success' };
+		global.fetch.mockResolvedValueOnce({
+			ok: true,
+			json: async () => (mockData)
+		})
+
+		const { result } = renderHook(() => useFetchData('/mock-url'));
+
+		await waitFor(() => {
+			expect(result.current.data).toEqual(mockData);
+			expect(result.current.loading).toBe(false);
+			expect(result.current.error).toBeNull();
+		})
+	});
+
+	it("throws an error when fetch fails", async () => {
+		const mockError = new Error("Fetch failed");
+		global.fetch.mockRejectedValueOnce(mockError);
+
+		const { result } = renderHook(() => useFetchData('/mock-url'));
+
+		await waitFor(() => {
+			expect(result.current.data).toBeNull();
+			expect(result.current.loading).toBe(false);
+			expect(result.current.error).toEqual(mockError);
+		})
+	});
+
+	it("aborts on unmount", () => {
+		global.fetch.mockImplementationOnce(() => {
+			return new Promise((res, reject) => {
+				setTimeout(() => reject(new DOMException('AbortError', 'AbortError')), 100)
+			})
+		});
+
+		const { result, unmount } = renderHook(() => useFetchData('/mock-url'));
+
+		unmount();
+
+		expect(result.current.data).toBeNull();
+		expect(result.current.error).toBeNull();
+	});
+
+})
