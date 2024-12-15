68c1ca1961b1cb9ffc6cb348c18f99a22fb067c1
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-12-15 14:50:19 +0100

    feat: fetches initial ship data to be passed to the data provider upon first page load

--- src/components/useFetchStarships/useFetchStarships.jsx
+++ src/components/useFetchStarships/useFetchStarships.jsx
@@ -1,13 +1,13 @@
 import { useState, useEffect } from "react";
 import useFetchData from "../useFetchData/useFetchData";
-import shipUrls from "./shipUrls";
+import { featureShips } from "./shipUrls";
 // TODO: Cache data after cache/memo lesson
 // TODO: Maybe add loading state through this instead of using loading state 
 // as standard state if data is not yet present
 export default function useFetchStarships() {
 	const [data, setData] = useState(null);
 	// Fetch ship data
-	const { data: shipData, error } = useFetchData(shipUrls);
+	const { data: shipData, error } = useFetchData(featureShips);
 	useEffect(() => {
 		if (shipData) {
 			setData(shipData);
