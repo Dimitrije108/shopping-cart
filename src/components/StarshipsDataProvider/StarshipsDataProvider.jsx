1515a09d4c232bf998be79a8142590ef55cb7a48
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-12-15 14:39:38 +0100

    feat: added state to manage and provide valid data to components

--- src/components/StarshipsDataProvider/StarshipsDataProvider.jsx
+++ src/components/StarshipsDataProvider/StarshipsDataProvider.jsx
@@ -1,20 +1,22 @@
 import { useState, createContext, useContext, useEffect  } from "react";
+import useFetchStarships from "../useFetchStarships/useFetchStarships";
 
 const Context = createContext();
 // Fetching data and using the provider pattern
 export const StarshipDataProvider = ({ children }) => {
-	const [starships, setStarships] = useState([]);
+	const [starships, setStarships] = useState(null);
+
+	const { data, error } = useFetchStarships();
 
 	useEffect(() => {
 		if (data && data.length > 0) {
 			setStarships(data);
+			console.log(data);
 		}
 	}, [data]);
 
-	console.log(data);
-
 	return (
-		<Context.Provider value={starships}>
+		<Context.Provider value={{starships, error}}>
 			{children}
 		</Context.Provider>
 	)
