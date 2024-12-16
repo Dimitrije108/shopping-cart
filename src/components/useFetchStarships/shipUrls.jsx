7eaa06173d84c88897595a6e071bb917f7fb41be
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-12-16 13:21:44 +0100

    feat: add all ship url arrays to be imported and fetched where needed

---
+++ src/components/useFetchStarships/shipUrls.jsx
@@ -0,0 +1,46 @@
+// Ships to be fetched
+const featureShips = [
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1d9e",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1dc2",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1d42",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1d65",
+];
+
+const capitalShips = [
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1d9f",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1d6d",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1dbe",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1dd8",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1dc3",
+];
+
+const transportShips = [
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1d8f",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1dc2",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1dc1",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1d9d",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1d9e",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1ddb",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1d8a",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1df8",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1d3c",
+];
+
+const starfighters = [
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1d6c",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1e3c",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1db9",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1d65",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1df3",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1dc0",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1df6",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1d42",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1df9",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1d67",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1d3a",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1d4d",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1e38",
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1e3e",
+];
+
+export { featureShips, capitalShips, transportShips, starfighters };
