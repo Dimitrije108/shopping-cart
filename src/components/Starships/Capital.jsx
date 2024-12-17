186f8124981f884e27a35127e8924ba11cfe6e96
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-12-17 17:56:39 +0100

    feat: add ShipCard component and implement it in the Capital component to display Capital ship data

--- src/components/Starships/Capital.jsx
+++ src/components/Starships/Capital.jsx
@@ -1,7 +1,30 @@
 import { useState } from 'react';
+import useFetchData from '../../hooks/useFetchData/useFetchData';
+import { capitalShips } from '../../shipUrls';
+import ShipCard from './ShipCard/ShipCard';
+import styles from './Starships.module.css';
 
 export default function Capital() {
+	const { data, error, loading } = useFetchData(capitalShips);
+
+	console.log(data);
+	console.log(error);
+
   return (
-		<div>This is Capital ships store page!</div>
+		<div>
+			<h1>This is Capital ships store page!</h1>
+			{loading && <div>Loading...</div>}
+			<div>
+				{data && data.map((ship) => {
+					return (
+						<ShipCard 
+							key={ship._id}
+							name={ship.name}
+							img={ship.image}
+						/>
+					)
+				})}
+			</div>
+		</div>
 	)
 };
--- src/components/Starships/ShipCard/ShipCard.jsx
+++ src/components/Starships/ShipCard/ShipCard.jsx
@@ -1,10 +1,14 @@
+import styles from "./ShipCard.module.css"
 // Create a ship card component
 export default function ShipCard({ name, img }) {
   return (
-		<div
-			style={{ backgroundImage: `url(${img})` }}
-		>
-			<h1>{name}</h1>
+		<div className={styles.shipCard}>
+			<div
+				className={styles.imgCont}
+				style={{ backgroundImage: `url(${img})` }}
+			>
+			</div>
+			<h1 className={styles.name}>{name}</h1>
 		</div>
 	)
 };
