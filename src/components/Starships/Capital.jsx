dfb9b0e1b917f8a49bcfdc600e2be18e9ce63e40
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-12-18 14:19:09 +0100

    feat: add Capital component functionality to display data, error or skeleton loading depending on the state of the fetch

--- src/components/Starships/Capital.jsx
+++ src/components/Starships/Capital.jsx
@@ -1,20 +1,24 @@
 import { useState } from 'react';
 import useFetchData from '../../hooks/useFetchData/useFetchData';
 import { capitalShips } from '../../shipUrls';
+import SkeletonCard from './SkeletonCard/SkeletonCard';
 import ShipCard from './ShipCard/ShipCard';
 import styles from './Starships.module.css';
 
 export default function Capital() {
-	const { data, error, loading } = useFetchData(capitalShips);
-
-	console.log(data);
-	console.log(error);
+	const { data, error } = useFetchData(capitalShips);
 
   return (
 		<div>
 			<h1>This is Capital ships store page!</h1>
-			{loading && <div>Loading...</div>}
 			<div>
+				{/* TODO: Make a separate component for error and display a button to retry the fetch */}
+				{error && <h1>Oops! Something went wrong. Server failed to load data.</h1>}
+				{/* Display skeleton loader until data fetches */}
+				{!data && !error && capitalShips.map((url) => {
+					return <SkeletonCard key={url} />
+				})}
+				{/* Display cards once data is fetched */}
 				{data && data.map((ship) => {
 					return (
 						<ShipCard 
