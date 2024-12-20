5c2a1901b2e7f991d6f85393777b09cf09e4e261
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-12-20 18:52:58 +0100

    feat: add ShipCard component that renders individual ship cards based on provided data

--- src/components/Starships/ShipCard/ShipCard.jsx
+++ src/components/Starships/ShipCard/ShipCard.jsx
@@ -1,14 +1,29 @@
 import styles from "./ShipCard.module.css"
 // Create a ship card component
-export default function ShipCard({ name, img }) {
-  return (
+export default function ShipCard({ name, img, price }) {
+	return (
 		<div className={styles.shipCard}>
 			<div
 				className={styles.imgCont}
 				style={{ backgroundImage: `url(${img})` }}
 			>
 			</div>
-			<h1 className={styles.name}>{name}</h1>
+			<div className={styles.infoCont}>
+				<div className={styles.nameCont}>
+					<h2 className={styles.name}>{name}</h2> 
+					<div>
+						<p className={styles.imperial}>{name}</p>
+						<p className={styles.naboo}>{name}</p>
+					</div>
+				</div>
+				<div className={styles.priceCont}>
+					<div className={styles.price}>{price}</div>
+					<div className={styles.addCont}>
+						<p>credits</p>
+						<button>ADD</button>
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 };
