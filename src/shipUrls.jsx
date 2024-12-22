5b844be5b41694a2ca94156eb3d38168b9d1406e
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-12-22 15:16:13 +0100

    feat: add all arrays of url's with additional ship information

--- src/shipUrls.jsx
+++ src/shipUrls.jsx
@@ -14,6 +14,14 @@ const capitalShips = [
 	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1dc3",
 ];
 
+const capitalShipDetails = [
+	"https://www.swapi.tech/api/starships/3",
+	"https://www.swapi.tech/api/starships/15",
+	"https://www.swapi.tech/api/starships/27",
+	"https://www.swapi.tech/api/starships/63",
+	"https://www.swapi.tech/api/starships/23",
+];
+
 const transportShips = [
 	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1d8f",
 	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1dc2",
@@ -26,10 +34,22 @@ const transportShips = [
 	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1d3c",
 ];
 
+const transportShipDetails = [
+	"https://www.swapi.tech/api/starships/49",
+	"https://www.swapi.tech/api/starships/40",
+	"https://www.swapi.tech/api/starships/43",
+	"https://www.swapi.tech/api/starships/5",
+	"https://www.swapi.tech/api/starships/22",
+	"https://www.swapi.tech/api/starships/31",
+	"https://www.swapi.tech/api/starships/17",
+	"https://www.swapi.tech/api/starships/58",
+	"https://www.swapi.tech/api/starships/47",
+];
+
 const starfighters = [
+	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1db9",
 	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1d6c",
 	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1e3c",
-	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1db9",
 	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1d65",
 	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1df3",
 	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1dc0",
@@ -43,4 +63,29 @@ const starfighters = [
 	"https://starwars-databank-server.vercel.app/api/v1/vehicles/6429291f021f17e13fbc1e3e",
 ];
 
-export { featureShips, capitalShips, transportShips, starfighters };
+const starfighterDetails = [
+	"https://www.swapi.tech/api/starships/10",
+	"https://www.swapi.tech/api/starships/65",
+	"https://www.swapi.tech/api/starships/12",
+	"https://www.swapi.tech/api/starships/13",
+	"https://www.swapi.tech/api/starships/41",
+	"https://www.swapi.tech/api/starships/39",
+	"https://www.swapi.tech/api/starships/21",
+	"https://www.swapi.tech/api/starships/66",
+	"https://www.swapi.tech/api/starships/74",
+	"https://www.swapi.tech/api/starships/48",
+	"https://www.swapi.tech/api/starships/28",
+	"https://www.swapi.tech/api/starships/29",
+	"https://www.swapi.tech/api/starships/75",
+	"https://www.swapi.tech/api/starships/11",
+];
+
+export { 
+	featureShips,
+	capitalShips,
+	capitalShipDetails,
+	transportShips,
+	transportShipDetails,
+	starfighters,
+	starfighterDetails,
+};
