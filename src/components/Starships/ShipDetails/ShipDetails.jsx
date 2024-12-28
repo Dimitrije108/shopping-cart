c35dddcaae17815b7dc7c33579b7e8f671db9adb
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-12-28 13:14:54 +0100

    feat: make quantity input editable by user
    Apart from plus and minus buttons to adjust quantity, user can now input the value manually as well.

--- src/components/Starships/ShipDetails/ShipDetails.jsx
+++ src/components/Starships/ShipDetails/ShipDetails.jsx
@@ -5,7 +5,7 @@ export default function ShipDetails({ basic, details }) {
 	const [quantity, setQuantity] = useState(1);
 
 	function increaseQuantity() {
-		if (quantity < 10) {
+		if (quantity < 20) {
 			setQuantity(quantity + 1);
 		}
 	}
@@ -13,6 +13,26 @@ export default function ShipDetails({ basic, details }) {
 	function decreaseQuantity() {
 		if (quantity > 1) {
 			setQuantity(quantity - 1);
+		}
+	}
+
+	function adjustQuantity(e) {
+		const value = e.target.value;
+		// Set the input to empty if user tries to write the value manually
+		if (value === "") {
+			setQuantity("");
+		}
+		// Set the inputed value
+		if (value > 0 && value <= 20) {
+			setQuantity(Number(value));
+		}
+	}
+
+	function checkQuantity() {
+		console.log("does it trigger")
+		// Reset the value back to 1 if it was left empty
+		if (quantity === "") {
+			setQuantity(1);
 		}
 	}
 	// Convert all unknown, empty or n/a values into N/A for consistency
@@ -39,37 +59,45 @@ export default function ShipDetails({ basic, details }) {
 			<div className={styles.imgCont}>
 				<img src={basic.image} />
 			</div>
-			<div className={styles.addCont}>
-				<div className={styles.priceCont}>
-					<div>{truePrice}</div>
-					{truePrice !== "Price on Request" && <p>credits</p>}
-				</div>
-				<div className={styles.financeCont}>
-					<div className={styles.financeOffer}>
-						<p>Finance Offer</p>
-						{truePrice !== "Price on Request" && <p>{`${truePrice / 24}/month`}</p>}
+			<div className={styles.addCol}>
+				<div  className={styles.addWrapper}>
+					<div className={styles.priceCont}>
+						<div>{truePrice}</div>
+						{truePrice !== "Price on Request" && <p>credits</p>}
 					</div>
-					<button>Get Pre-Qualified</button>
+					<div className={styles.financeCont}>
+						<div className={styles.financeOffer}>
+							<p>Finance Offer</p>
+							{truePrice !== "Price on Request" && 
+								<p className={styles.offerPrice}>
+									{`${(truePrice / 24).toFixed(2)}/month`}
+								</p>
+							}
+						</div>
+						<button>Get Pre-Qualified</button>
+					</div>
+					<div className={styles.quantityCont}>
+						<button 
+							className={styles.quantityLeftBtn}
+							onClick={decreaseQuantity}
+						>-</button>
+						<input 
+							className={styles.quantityInput}
+							type="number"
+							value={quantity}
+							min={1}
+							max={20}
+							onChange={adjustQuantity}
+							onBlur={checkQuantity}
+						>
+						</input>
+						<button
+							className={styles.quantityRightBtn}
+							onClick={increaseQuantity}
+						>+</button>
+					</div>
+					<button className={styles.addBtn}>ADD</button>
 				</div>
-				<div className={styles.quantityCont}>
-					<button 
-						className={styles.quantityLeftBtn}
-						onClick={decreaseQuantity}
-					>-</button>
-					<input 
-						className={styles.quantityInput}
-						type="number"
-						value={quantity}
-						min={1}
-						max={10}
-					>
-					</input>
-					<button
-						className={styles.quantityRightBtn}
-						onClick={increaseQuantity}
-					>+</button>
-				</div>
-				<button className={styles.addBtn}>ADD</button>
 			</div>
 			<div className={styles.descriptionCont}>
 				<h1>{basic.name}</h1>
@@ -120,3 +148,28 @@ export default function ShipDetails({ basic, details }) {
 		</div>
 	)
 };
+
+// TODO: add info about financial offer, like:
+// *The Estimated Monthly Payment is based on a 72-month term, a 
+// down payment of $32,140, Annual Percentage Rate(APR) of 9.99%, 
+// estimated taxes and fees in the amount of $289,260, and no trade-in.
+
+// TODO: below that there can be a contact the dealer form. like:
+// Contact the Dealer
+// Call +1(888) 848-1168
+// First Name 
+// Last Name 
+// Email 
+// Phone Number 
+// I am interested in this 2024 Aston Martin DB12
+
+// Free Email Newsletter
+
+
+// Contact me with an Insurance Quote
+
+
+// Contact me with a Shipping Quote
+
+// The finest exotic cars & sports cars for sale | duPont REGISTRYCheck Availability
+// By pressing this button I agree to the Terms of Use and Privacy Policy.
