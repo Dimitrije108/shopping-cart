73d72cd14d66b7bf8ba8a3c90c365572671c1a4c
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-12-28 11:19:00 +0100

    feat: add tables for additional information, quantity field and value checking logic
    Tables better represent data. Quantity field let's you specify the amount of items to buy. Logic for checking values makes empty, unkown and n/a returns display uniformly.

--- src/components/Starships/ShipDetails/ShipDetails.jsx
+++ src/components/Starships/ShipDetails/ShipDetails.jsx
@@ -1,79 +1,122 @@
+import { useState } from "react";
 import styles from "./ShipDetails.module.css";
 
 export default function ShipDetails({ basic, details }) {
+	const [quantity, setQuantity] = useState(1);
+
+	function increaseQuantity() {
+		if (quantity < 10) {
+			setQuantity(quantity + 1);
+		}
+	}
+
+	function decreaseQuantity() {
+		if (quantity > 1) {
+			setQuantity(quantity - 1);
+		}
+	}
+	// Convert all unknown, empty or n/a values into N/A for consistency
+	const info = Object.fromEntries(
+		Object.entries(details).map(([key, val]) => {
+			if (
+				val === "" || 
+				val === "unknown" ||
+				val === "n/a"
+			) {
+				return [key, "N/A"];
+			} else {
+				return [key, val];
+			}
+	}));
+	// Convert unknown price into "Price on Request" to better suit the
+	// eCommerce nature of the project
 	const truePrice = details.cost_in_credits === "unknown" 
 		? "Price on Request" 
 		: details.cost_in_credits;
 
   return (
 		<div className={styles.shipDetailsCont}>
-			<div className={styles.leftCont}>
-				<div className={styles.imgCont}>
-					<img 
-						src={basic.image}
-						height={400}
-					/>
-				</div>
-				<div className={styles.priceAndDetailsCont}>
-					<div>
-						<div>
-							<div>{truePrice}</div>
-							{truePrice !== "Price on Request" && <p>credits</p>}
-						</div>
-						<button>ADD SHIP</button>
-					</div>
-					<div className={styles.detailsCont}>
-						<div>
-							<p>Model: </p>
-							<div>{details.model}</div>
-						</div>
-						<div>
-							<p>Starship class: </p>
-							<div>{details.straship_class}</div>
-						</div>
-						<div>
-							<p>Manufacturer: </p>
-							<div>{details.manufacturer}</div>
-						</div>
-						<div>
-							<p>Length: </p>
-							<div>{details.length}</div>
-						</div>
-						<div>
-							<p>Crew: </p>
-							<div>{details.crew}</div>
-						</div>
-						<div>
-							<p>Passengers: </p>
-							<div>{details.passengers}</div>
-						</div>
-						<div>
-							<p>Max athmospheric speed: </p>
-							<div>{details.max_atmosphering_speed}</div>
-						</div>
-						<div>
-							<p>Hyperdrive rating: </p>
-							<div>{details.hyperdrive_rating}</div>
-						</div>
-						<div>
-							<p>MGLT: </p>
-							<div>{details.MGLT}</div>
-						</div>
-						<div>
-							<p>Cargo capacity: </p>
-							<div>{details.cargo_capacity}</div>
-						</div>
-						<div>
-							<p>Consumables: </p>
-							<div>{details.consumables}</div>
-						</div>
-					</div>
-				</div>
+			<div className={styles.imgCont}>
+				<img src={basic.image} />
 			</div>
-			<div className={styles.rightCont}>
+			<div className={styles.addCont}>
+				<div className={styles.priceCont}>
+					<div>{truePrice}</div>
+					{truePrice !== "Price on Request" && <p>credits</p>}
+				</div>
+				<div className={styles.financeCont}>
+					<div className={styles.financeOffer}>
+						<p>Finance Offer</p>
+						{truePrice !== "Price on Request" && <p>{`${truePrice / 24}/month`}</p>}
+					</div>
+					<button>Get Pre-Qualified</button>
+				</div>
+				<div className={styles.quantityCont}>
+					<button 
+						className={styles.quantityLeftBtn}
+						onClick={decreaseQuantity}
+					>-</button>
+					<input 
+						className={styles.quantityInput}
+						type="number"
+						value={quantity}
+						min={1}
+						max={10}
+					>
+					</input>
+					<button
+						className={styles.quantityRightBtn}
+						onClick={increaseQuantity}
+					>+</button>
+				</div>
+				<button className={styles.addBtn}>ADD</button>
+			</div>
+			<div className={styles.descriptionCont}>
 				<h1>{basic.name}</h1>
 				<p>{basic.description}</p>
 			</div>
+			<table className={styles.infoTable1}>
+				<thead>
+					<tr>
+						<th>Model</th>
+						<th>Starship class</th>
+						<th>Manufacturer</th>
+						<th>Length</th>
+						<th>Crew</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>{info.model}</td>
+						<td>{info.starship_class}</td>
+						<td>{info.manufacturer}</td>
+						<td>{info.length}</td>
+						<td>{info.crew}</td>
+					</tr>
+				</tbody>
+			</table>
+			<table className={styles.infoTable2}>
+				<thead>
+					<tr>
+						<th>Passengers</th>
+						<th>Max athmospheric speed</th>
+						<th>Hyperdrive rating</th>
+						<th>MGLT</th>
+						<th>Cargo capacity</th>
+						<th>Consumables</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>{info.passengers}</td>
+						<td>{info.max_atmosphering_speed}</td>
+						<td>{info.hyperdrive_rating}</td>
+						<td>{info.MGLT}</td>
+						<td>{info.cargo_capacity}</td>
+						<td>{info.consumables}</td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	)
 };
