a259232db1e1cc4e3588aeacd2e7ae9d411c485e
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-12-29 12:33:35 +0100

    feat: add skeleton loading page component and styling for ship details page

---
+++ src/components/Starships/ShipInfoPage/SkeletonDetails/SkeletonDetails.jsx
@@ -0,0 +1,21 @@
+import styles from "./SkeletonDetails.module.css"
+// Create a ship card component
+export default function SkeletonDetails() {
+	return (
+		<div className={styles.skelPage}>
+			<div className={styles.skelImg}></div>
+			<div  className={styles.descCont}>
+				<div className={styles.name}></div>
+				<div className={styles.description}></div>
+			</div>
+			<div className={styles.price}></div>
+			<div  className={styles.tableCont}>
+				<div className={styles.spec}></div>
+				<div className={styles.skelTable}>
+					<div className={styles.table}></div>
+					<div className={styles.table}></div>
+				</div>
+			</div>
+		</div>
+	)
+};
