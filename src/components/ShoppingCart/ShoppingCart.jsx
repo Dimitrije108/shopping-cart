069527b2151614160bee6fc08be213f4c94b8364
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-01-07 20:16:59 +0100

    feat: display all shopping cart items in the shopping cart component

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,7 +1,28 @@
-import { useState } from 'react';
+import { useCartContext } from '../../hooks/useShoppingCart/useShoppingCart';
+import CartItem from './CartItem/CartItem';
+import styles from './ShoppingCart.module.css';
 
 export default function ShoppingCart() {
+	const { cart } = useCartContext();
+
   return (
-		<div>This is where your items show up so you can buy them!</div>
+		<>
+			<h1>Shopping Cart</h1>
+			{cart.map((ship) => {
+				return (
+					<CartItem
+						key={ship.id}
+						id={ship.id}
+						img={ship.img}
+						name={ship.name}
+						price={ship.price}
+						shipQuantity={ship.quantity}
+					/>
+				)
+			})}
+			{/* total price of all ships:
+			1. iterate over cart and for each ship do quantity x price
+			2. add all prices together, maybe via reduce method? */}
+		</>
 	)
 };
