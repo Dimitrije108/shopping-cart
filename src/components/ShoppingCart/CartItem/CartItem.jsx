9545f335d513412e3fb3db1c4b6a2eac5515b744
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-01-09 17:47:40 +0100

    feat: implement adjusted item quantity logic for the shopping cart

--- src/components/ShoppingCart/CartItem/CartItem.jsx
+++ src/components/ShoppingCart/CartItem/CartItem.jsx
@@ -4,10 +4,9 @@ import ItemQuantity from '../../ItemQuantity/ItemQuantity';
 import styles from './CartItem.module.css';
 
 export default function CartItem({ id, img, name, price, shipQuantity }) {
-	const {  
-		limitExceeded, 
-		limitExceededMsg, 
-		removeFromCart,
+	const { 
+		removeFromCart, 
+		adjustItemQuantity, 
 	} = useCartContext();
 
 	const {
@@ -18,41 +17,54 @@ export default function CartItem({ id, img, name, price, shipQuantity }) {
 		resetQuantity,
 	} = useItemQuantity(shipQuantity);
 
+	const handleQuantityChange = (newQuan) => {
+		adjustItemQuantity(id, newQuan);
+	};
+
 	// img and name are clickable links that lead to respective ship info pages
 
 	return (
-		<div className={styles.cartItem}>
-			<div className={styles.imgCont}>
-				<img src={img} alt={`${name} starship`} />
-			</div>
-			<div className={styles.nameCont}>
-				<h2 className={styles.name}>{name}</h2> 
-				<div>
-					<p className={styles.imperial}>{name}</p>
-					<p className={styles.naboo}>{name}</p>
+		<div  className={styles.cartItemCont}>
+			<hr />
+			<div className={styles.cartItem}>
+				<div className={styles.imgCont}>
+					<img src={img} alt={`${name} starship`} />
 				</div>
+				<div className={styles.nameCont}>
+					<h2 className={styles.name}>{name}</h2> 
+					<div>
+						<p className={styles.imperial}>{name}</p>
+						<p className={styles.naboo}>{name}</p>
+					</div>
+				</div>
+				<ItemQuantity
+					quantity={quantity}
+					increase={() => increaseQuantity(handleQuantityChange)}
+					decrease={() => decreaseQuantity(handleQuantityChange)}
+					change={(e) => changeQuantity(e, handleQuantityChange)}
+					reset={() => resetQuantity(handleQuantityChange)}
+				/>
+				<div className={styles.priceCont}>
+					<button 
+						className={styles.delBtn}
+						onClick={() => removeFromCart(id)}
+					>X</button>
+					<p>
+						{price === "Price on Request" 
+						? price
+						: <>
+								<span className={styles.itemQuantity}>{`${quantity}x `}</span>{quantity * price}
+							</>
+						}
+					</p>
+				</div>
+				{/* needs to show up as a warning next to a ship that user tried to add
+				to the cart */}
+				{/* considering quantity inside cart can be changed only via input it doesn't
+				need the warning as it can't exceed the limit of 20, right? It also should 
+				be put only where addToCart is called */}
+				{/* {limitExceeded && limitExceededMsg} */}
 			</div>
-			<ItemQuantity
-				quantity={quantity}
-				increase={increaseQuantity}
-				decrease={decreaseQuantity}
-				change={changeQuantity}
-				reset={resetQuantity}
-			/>
-			{/* price: 2x 100,000 */}
-			<div className={styles.priceCont}>
-				{price === "Price on Request" ? (
-					<p>{price}</p>
-				) : (
-					<p>{`${quantity}x ${quantity * price} credits`}</p>
-				)}
-			</div>
-			{/* needs to show up as a warning next to a ship that user tried to add
-			to the cart */}
-			{/* considering quantity inside cart can be changed only via input it doesn't
-			need the warning as it can't exceed the limit of 20, right? It also should 
-			be put only where addToCart is called */}
-			{/* {limitExceeded && limitExceededMsg} */}
 		</div>
 	)
 };
