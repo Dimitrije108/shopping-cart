5a9091a808d1e884fffe68bdc17e4198766ba668
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-01-15 15:04:48 +0100

    feat: implement PopupContext provider with cart popup functionality for the whole app

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
 import { Outlet, useLocation } from "react-router-dom";
-import CartContext, { useShoppingCart } from "./hooks/useShoppingCart/useShoppingCart";
+import CartContext, { useShoppingCart } from "./hooks/useCartContext/useCartContext";
+import PopupContext, { useAddToCartPopup } from "./hooks/useCartPopup/useCartPopup";
 import Header from "./components/Header";
 import Homepage from "./components/Homepage";
 import Footer from "./components/Footer";
+import CartPopups from "./components/CartPopups/CartPopups";
 import styles from "./layout.module.css"
 
 export function App() {
   const {pathname} = useLocation();
   const cartFunctionality = useShoppingCart();
+  const popupFunctionality = useAddToCartPopup();
 
   return (
     <CartContext.Provider value={cartFunctionality}>
-      <div className={styles.layoutWrapper}>
-        <Header />
-        {pathname === "/" && <Homepage />}
-        <Outlet />
-        <Footer />
-      </div>
+      <PopupContext.Provider value={popupFunctionality}>
+        <div className={styles.layoutWrapper}>
+          <Header />
+          {pathname === "/" && <Homepage />}
+          <Outlet />
+          <Footer />
+          <CartPopups />
+        </div>
+      </PopupContext.Provider>
     </CartContext.Provider>
   )
 };
