fb2f14b554aad3122308346bbc97c0ac086703e6
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-01-20 12:30:52 +0100

    feat: add empty shopping cart page content
    Including a cart svg icon and a link redirecting the user to the shopping page.

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,28 +1,47 @@
-import { useCartContext } from '../../hooks/useShoppingCart/useShoppingCart';
+import { Link } from "react-router-dom";
+import { useCartContext } from '../../hooks/useCartContext/useCartContext';
 import CartItem from './CartItem/CartItem';
+import CartSummary from './CartSummary/CartSummary';
 import styles from './ShoppingCart.module.css';
 
 export default function ShoppingCart() {
 	const { cart } = useCartContext();
+	let cartItems;
+
+	if (cart.length < 1) {
+		cartItems = 
+			<div className={styles.emptyCartCont}>
+				<div>
+					<img src="/src/assets/cart.svg" alt="shopping cart icon" width={150} height={150} />
+				</div>
+				<p>Seems like you haven't added any items to your cart.</p>
+				<Link 
+					to={"/starships"} 
+					className={styles.shopLink}>
+						Shop here!
+				</Link>
+			</div>
+	} else {
+		cartItems = cart.map((ship) => (
+				<CartItem
+					key={ship.id}
+					id={ship.id}
+					img={ship.img}
+					name={ship.name}
+					category={ship.category}
+					price={ship.price}
+					shipQuantity={ship.quantity}
+				/>
+		))
+	};
 
   return (
 		<>
 			<h1>Shopping Cart</h1>
-			{cart.map((ship) => {
-				return (
-					<CartItem
-						key={ship.id}
-						id={ship.id}
-						img={ship.img}
-						name={ship.name}
-						price={ship.price}
-						shipQuantity={ship.quantity}
-					/>
-				)
-			})}
-			{/* total price of all ships:
-			1. iterate over cart and for each ship do quantity x price
-			2. add all prices together, maybe via reduce method? */}
+			<div className={styles.cartWrapper}>
+				<div>{cartItems}</div>
+				<CartSummary />
+			</div>
 		</>
 	)
 };
