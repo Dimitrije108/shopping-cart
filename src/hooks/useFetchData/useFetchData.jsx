016c3cb63e3e4fb313b068b71590b7ecfbadf7e0
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-01-21 11:13:25 +0100

    feat: implement data caching with localStorage before fetching that will refetch every 4 hours
    Check local storage for url data before fetching, if data is younger than 4 hours it will be provided, otherwise it is refetched.

--- src/hooks/useFetchData/useFetchData.jsx
+++ src/hooks/useFetchData/useFetchData.jsx
@@ -4,6 +4,8 @@ export default function useFetchData(arr) {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
 	const [loading, setLoading] = useState(true);
+	// 4 hours in miliseconds
+	const cacheDuration = 4 * 60 * 60 * 1000;
 
   useEffect(() => {
 		// Turn param into an array if it isn't already
@@ -14,13 +16,28 @@ export default function useFetchData(arr) {
 			// Fetch all urls from the array simultaneously
 			Promise
 				.all(
-					urls.map((url) => 
-						fetch(url, { signal: controller.signal })
+					urls.map((url) => {
+						// Check and return data if it is cached
+						const cached = localStorage.getItem(url);
+						if (cached) {
+							const { data, timestamp } = JSON.parse(cached);
+							// Return data if 4h haven't expired
+							if (Date.now() - timestamp < cacheDuration) {
+								return data;
+							}
+						};
+						// If it is not cached fetch and store in localStorage
+						return fetch(url, { signal: controller.signal })
 							.then((resp) => {
 								if (!resp.ok) {
 									throw new Error(`Status: ${resp.status} ${resp.statusText}`);
 								}
 								return resp.json();
+							})
+							.then((data) => {
+								// Store fetched data in localStorage
+								localStorage.setItem(url, JSON.stringify({ data, timestamp: Date.now() }));
+								return data;
 							})
 							.catch((error) => {
 								if (error.name === "AbortError") {
@@ -28,14 +45,17 @@ export default function useFetchData(arr) {
 									return null;
 								}
 								return { error: error.message, url };
-							}))
+							})
+					})
 				).then((resp) => {
 					const filterAbort = resp.filter((r) => r !== null);
+					
 					const successes = filterAbort.filter((r) => !r.error);
 					if (successes.length > 0) {
 						setData(successes);
 						setError(null);
 					}
+
 					const failures = filterAbort.filter((r) => r.error);
 					if (failures.length > 0) {
 						setError(failures);
