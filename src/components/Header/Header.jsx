457c1ac0cd12097d9189b7ecb16a6765ccc7da26
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-01-26 18:51:11 +0100

    feat: add and adjust Header content like navbar, links and element arangement
    Rearanged navbar elements due to positioning and functionality, added new cart icon and quantity display, github logo and other minor adjustments.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,62 +1,91 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useCartContext } from "../../hooks/useCartContext/useCartContext";
 import styles from "./Header.module.css";
 
 export default function Header() {
 	const [starshipsDropdown, setStarshipsDropdown] = useState(false);
+	const { pathname } = useLocation();
 	const { cart } = useCartContext();
 
 	const togglestarshipsDropdown = () => {
 		setStarshipsDropdown((prev) => !prev);
 	};
 
-	const starshipLinks = (
-		<ul className={styles.starshipsList}>
-			<li>
-				<Link to="starships/capital">Capital</Link>
-			</li>
-			<li>
-				<Link to="starships/transport">Transport</Link>
-			</li>
-			<li>
-				<Link to="starships/starfighter">Starfighter</Link>
-			</li>
-		</ul>
-	);
-
   return (
 		<div className={styles.headerCont}>
-			<h1  className={styles.logoHeading}>
-				<Link to="/" className={styles.logoLink}  title="Star Wars Shipyard">
+			<div className={styles.logoCont}>
+				<Link 
+					to="https://github.com/Dimitrije108/shopping-cart"
+					className={styles.githubLink}
+				>
 					<img 
-						src="/src/assets/sw-logo.png"  
-						alt="star wars logo" 
-						width={150}
+						src="/src/assets/github-mark-white.svg" 
+						alt="github icon" 
+						width={30}
+						title="GitHub"
 					/>
 				</Link>
-				<Link to="/" className={styles.shipyardLink} title="Star Wars Shipyard">
-					<div className={styles.shipyardLogo}>Shipyard</div>
+				<h1 className={styles.logoHeading}>
+					<Link to="/" className={styles.logoLink}>
+						<img 
+							src="/src/assets/sw-logo.png"  
+							alt="star wars logo" 
+							width={150}
+						/>
+					</Link>
+					<Link 
+						to="/" 
+						className={styles.shipyardLink} 
+						title="Shipyard"
+					>
+						<div className={styles.shipyardLogo}>Shipyard</div>
+					</Link>
+				</h1>
+				<Link 
+					to="shopping-cart"
+					className={styles.cartIconLink}
+				>
+					<img 
+						src="https://icons.iconarchive.com/icons/jonathan-rey/star-wars-vehicles/128/Death-Star-2nd-icon.png" 
+						width="55" 
+						height="55"
+					/>
+					<div className={styles.cartQuantity}>
+						<div className={styles.quantityWrapper}>{`${cart.length}`}</div>
+					</div>
 				</Link>
-			</h1>
+			</div>
 			<nav className={styles.navbarCont}>
+				<div></div>
 				<ul className={styles.navbar}>
-					<li>
+					<li 
+						className={`${styles.homeNav} ${pathname==="/" ? styles.active : ""} `}
+					>
 						<Link to="/">Home</Link>
 					</li>
 					<li 
-						className={styles.starshipsCont}
+						className={`${styles.starshipsNav} ${pathname.startsWith("/starships") ? styles.active : ""} `}
 						onMouseEnter={() => togglestarshipsDropdown()}
 						onMouseLeave={() => togglestarshipsDropdown()}
 					>
 						<Link to="starships">Starships</Link>
-						{starshipsDropdown && starshipLinks}
+						<ul className={`${styles.dropdown} ${starshipsDropdown ? styles.visible : ""}`}>
+							<li>
+								<Link to="starships/capital">Capital</Link>
+							</li>
+							<li>
+								<Link to="starships/transport">Transport</Link>
+							</li>
+							<li>
+								<Link to="starships/starfighter">Starfighter</Link>
+							</li>
+						</ul>
 					</li>
-					<li>
+					<li 
+						className={`${styles.contactNav} ${pathname.startsWith("/contact") ? styles.active : ""} `}
+					>
 						<Link to="contact">Contact</Link>
-					</li>
-					<li>
-						<Link to="shopping-cart">Cart{`(${cart.length})`}</Link>
 					</li>
 				</ul>
 			</nav>
