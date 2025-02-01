fbbf0de95f684fb0b6a6aba6e6aed9a9bc8d6cdf
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-02-01 17:58:13 +0100

    feat: add Large Order Contact Form and Request Ship Price form sections and partners section content

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styles from "./Contact.module.css";
 
 export default function Contact() {
@@ -5,7 +6,9 @@ export default function Contact() {
 		<>
 			<div className={styles.wrapper}>
 				{/* <div  className={styles.backdrop}></div> */}
-				<h1>Contact Us</h1>
+				<h1 className={styles.findUsHeading}>
+					Find Us
+				</h1>
 				<div className={styles.locationCont}>
 					<img 
 						src="/src/assets/galaxy_map.jpg" 
@@ -59,16 +62,213 @@ export default function Contact() {
 						</div>
 					</div>
 				</div>
+				<h1 className={styles.contactUsHeading}>
+					Contact Us
+				</h1>
 				<div className={styles.contactFormsCont}>
-					<div className={styles.largeOrderForm}>
-
+					<div className={styles.largeOrderFormCont}>
+						<h2 className={styles.contactFormHeading}>
+							Large Order Contact Form
+						</h2>
+						<form action="" className={styles.largeOrderForm}>
+							<div className={styles.inputCont}>
+								<label htmlFor="firstName">
+									First name*
+								</label>
+								<input
+									id="firstName"
+									name="firstName"
+									placeholder="Anakin"
+									minLength={2}
+									maxLength={18}
+									required
+								/>
+							</div>
+							<div className={styles.inputCont}>
+								<label htmlFor="lastName">
+									Last name*
+								</label>
+								<input
+									id="lastName"
+									name="lastName"
+									placeholder="Skywalker"
+									minLength={2}
+									maxLength={18}
+									required
+								/>
+							</div>
+							<div className={styles.inputCont}>
+								<label htmlFor="mail">
+									Contact email*
+								</label>
+								<input
+									id="mail"
+									type="email"
+									name="mail"
+									placeholder="AniSkywalker@tatooine.galaxy"
+									minLength={2}
+									maxLength={40}
+									required
+								/>
+							</div>
+							<div className={styles.inputCont}>
+								<label htmlFor="company">
+									Company
+								</label>
+								<input
+									id="company"
+									name="company"
+									placeholder="Jedi Order"
+									minLength={2}
+									maxLength={18}
+								/>
+							</div>
+							<div className={styles.inputCont}>
+								<label htmlFor="message">
+									Request*
+								</label>
+								<textarea
+									id="message"
+									name="message"
+									placeholder="Large order request message..."
+									className={styles.messageTextarea}
+									required
+								/>
+							</div>
+							<button
+								type="submit"
+								className={styles.submitFormBtn}
+							>
+								Submit
+							</button>
+						</form>
 					</div>
-					<div className={styles.requestPriceForm}>
-						
+					<div className={styles.requestPriceFormCont}>
+						<h2 className={styles.contactFormHeading}>
+							Request Ship Price
+						</h2>
+						<form action="" className={styles.requestPriceForm}>
+							<div  className={styles.inputCont}>
+								<label htmlFor="firstName">
+									First name*
+								</label>
+								<input
+									id="firstName"
+									name="firstName"
+									placeholder="Anakin"
+									minLength={2}
+									maxLength={18}
+									required
+								/>
+							</div>
+							<div className={styles.inputCont}>
+								<label htmlFor="lastName">
+									Last name*
+								</label>
+								<input
+									id="lastName"
+									name="lastName"
+									placeholder="Skywalker"
+									minLength={2}
+									maxLength={18}
+									required
+								/>
+							</div>
+							<div className={styles.inputCont}>
+								<label htmlFor="mail">
+									Contact email*
+								</label>
+								<input
+									id="mail"
+									type="email"
+									name="mail"
+									placeholder="AniSkywalker@tatooine.galaxy"
+									minLength={2}
+									maxLength={25}
+									required
+								/>
+							</div>
+							<div className={styles.inputCont}>
+								<label htmlFor="company">
+									Company
+								</label>
+								<input
+									id="company"
+									name="company"
+									placeholder="Jedi Order"
+									minLength={2}
+									maxLength={18}
+								/>
+							</div>
+							<div className={styles.inputCont}>
+								<label htmlFor="ships">
+									Request price*
+								</label>
+								<select 
+									id="ships"
+									name="ships"
+									required
+								>
+									<option value="">--Select ship--</option>
+									<option value="hTypeNubianYacht">H-type Nubian yacht</option>
+									<option value="nabooRoyalStarship">Naboo Royal Starship</option>
+									<option value="republicCruiser">Republic Cruiser</option>
+									<option value="gr75MediumTransport">GR-75 Medium Transport</option>
+									<option value="aa9CoruscantFreighter">AA-9 Coruscant freighter</option>
+									<option value="darthVadersTieFighter">Darth Vader's TIE fighter</option>
+									<option value="slaveI">Slave I</option>
+								</select>
+							</div>
+							<button
+								type="submit"
+								className={styles.submitFormBtn}
+							>
+								Submit
+							</button>
+						</form>
 					</div>
 				</div>
+				<h1 className={styles.partnersHeading}>
+					Partners
+				</h1>
 				<div className={styles.partnersCont}>
-
+					<div className={styles.logoWrapper}>
+						<div className={styles.logoCont}>
+							<Link 
+								to={"https://starwars.fandom.com/wiki/Confederation"}
+								data-testid="cfLink"
+							>
+								<img 
+									src="/src/assets/Corellian_Federation_logo.webp" 
+									alt="Corellian Federation icon" 
+									width={100}
+									title="Corellian Federation"
+								/>
+							</Link>
+							<Link 
+								to={"https://starwars.fandom.com/wiki/Kuat_Drive_Yards"}
+								data-testid="kdyLink"
+							>
+								<img 
+									src="/src/assets/Kuat-Drive-Yards.svg" 
+									alt="Kuat Drive Yards icon" 
+									width={100}
+									title="Kuat Drive Yards"
+								/>
+							</Link>
+							<Link 
+								to={"https://starwars.fandom.com/wiki/Fondor_Shipyards"}
+								data-testid="fondorLink"
+							>
+								<img 
+									src="/src/assets/Fondor-Shipyards.jpg" 
+									alt="Fondor Shipyards icon" 
+									width={100}
+									title="Fondor Shipyards"
+								/>
+							</Link>
+						</div>
+					</div>
 				</div>
 			</div>
 		</>
