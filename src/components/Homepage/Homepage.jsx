2932b2edb5662b39f74b3809682bc3657b4c3450
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-02-05 17:17:31 +0100

    feat: add Homepage component first page content

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styles from "./Homepage.module.css";
 import layout from "../../layout.module.css";
 
@@ -5,8 +6,41 @@ export default function Homepage() {
 
   return (
 		<>
+			<div 
+				className={styles.heroImage}
+			></div>
 			<div className={layout.layoutWrapper}>
-				<div>Welcome home, may the force be with you!</div>
+				<div className={styles.firstPageCont}>
+					<div className={styles.headerCont}>
+						<h1>
+							From Naboo to the Outer Rim — Corellian Engineering Excellence — Now in Your Hands!
+						</h1>
+						<Link to="/starships">
+							<button className={styles.shopBtn}>Shop Here!</button>
+						</Link>
+					</div>
+					<div className={styles.arrowContainer}>
+						<Link to="#toscroll">
+							<div className={styles.chevron}></div>
+							<div className={styles.chevron}></div>
+							<div className={styles.chevron}></div>
+							<span className={styles.text}>Scroll down</span>
+						</Link>
+					</div>
+				</div>
+				<div 
+					className="carouselCont"
+					id="toscroll"
+				>
+					<img 
+						src="/src/assets/star-destroyer.jpg" 
+						alt="Star Destroyer Capital ship"
+						className={styles.carouselImg}
+					/>
+					<div>
+						This gon be dat carouuseel!
+					</div>
+				</div>
 			</div>
 		</>
 	)
@@ -15,10 +49,10 @@ export default function Homepage() {
 // "Explore the Galaxy—One Starship at a Time."
 // "Built for the Stars, Engineered for You."
 // "From Naboo to the Outer Rim—We've Got Your Ship."
-//  "Hyperdrive-Ready. Are You?"
+// "Hyperdrive-Ready. Are You?"
 // "The Finest Ships in the Galaxy, Ready for You."
 
-// For Naboo luxuty ships?
+// For Naboo luxury ships:
 // "More Than a Ship—A Lifestyle."
 // "Own the Stars. Fly in Style."
 
@@ -28,4 +62,17 @@ export default function Homepage() {
 
 // "Unleash Your Inner Pilot."
 
+//  "Lightspeed Deals on Star Wars Ships!"
+// "From TIE Fighters to X-Wings—Own a Piece of the Galaxy."
 // May the force be with you!
+
+// For discounted prices
+// "Only a Sith Deals in Full Retail. Shop Our Discounts!"
+
+// Corellia
+// "Straight from the Corellian Shipyards—The Galaxy’s Finest Ships!"
+// "Corellian Engineering Excellence—Now in Your Hands!"
+//  "Built for Smugglers, Pilots, and Legends—Corellian Quality Guaranteed!"
+
+// For Millenium Falcon
+// "The Fastest Hunk of Junk? Not on Our Watch—Corellian Quality Inside!"
