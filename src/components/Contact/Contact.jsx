68205124b1cf67779d5a7e089ec9f42682a76621
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-02-13 14:20:08 +0100

    feat: page always loads with scrollbar at the top for better UX

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Contact.module.css";
 import layout from "../../layout.module.css";
 
 export default function Contact() {
+	// Scroll to top on load
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
+
   return (
 		<>
 			<div
