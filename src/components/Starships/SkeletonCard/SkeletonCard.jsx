9ff66b0dd5659938c4a8dfafd2d31566c74e946c
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-03-02 13:35:25 +0100

    feat: adjust skeleton card elements to have responsive design for all screen sizes

--- src/components/Starships/SkeletonCard/SkeletonCard.jsx
+++ src/components/Starships/SkeletonCard/SkeletonCard.jsx
@@ -5,14 +5,18 @@ export default function SkeletonCard() {
 		<div className={styles.skelCard} data-testid="skelCard">
 			<div className={styles.skelImg}></div>
 			<div className={styles.infoCont}>
-				<div className={styles.skelName}>
-					<div className={styles.name}></div>
-					<div className={styles.imperial}></div>
-					<div className={styles.naboo}></div>
+				<div  className={styles.infoWrapper}>
+					<div className={styles.skelName}>
+						<div className={styles.name}></div>
+						<div className={styles.imperial}></div>
+						<div className={styles.naboo}></div>
+					</div>
+					<div className={styles.buyCont}>
+						<div className={styles.price}></div>
+						<div className={styles.credits}></div>
+					</div>
 				</div>
-				<div className={styles.buyCont}>
-					<div className={styles.price}></div>
-					<div className={styles.credits}></div>
+				<div className={styles.addToCartCont}>
 					<div className={styles.addBtn}></div>
 				</div>
 			</div>
