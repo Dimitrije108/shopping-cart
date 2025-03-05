0c29c00e95fdfdd30ada50d67bff976d7807bb03
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-03-05 16:03:22 +0100

    feat: adjust positioning and styling and add responsive design to Error Page

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -4,10 +4,12 @@ import styles from "./ErrorPage.module.css";
 export default function ErrorPage() {
 	return (
 		<div className={styles.errorCont}>
-			<img 
-				src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZzRxZjhtcnM0Z2dwcGg2d21wdnRqYW92N3hzNGRuNTgwcnZtZ2N2NSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3ornka9rAaKRA2Rkac/giphy.gif" 
-				alt="It's a trap! - Admiral Ackbar gif"
-			/>
+			<div className={styles.gifCont}>
+				<img 
+					src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZzRxZjhtcnM0Z2dwcGg2d21wdnRqYW92N3hzNGRuNTgwcnZtZ2N2NSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3ornka9rAaKRA2Rkac/giphy.gif" 
+					alt="It's a trap! - Admiral Ackbar gif"
+				/>
+			</div>
 			<h1>This route doesn't exist!</h1>
 			<Link to="/">
 				Click here to go back to the homepage!
